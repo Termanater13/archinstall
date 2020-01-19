eefc2ed2c36c7758fb4b45d8ff87f3bab9402a8c
Stephen Pierce <Termanater13@users.noreply.github.com>
2020-01-19 09:34:14 -0500

    Update install.sh
    convert space to tab as a way ti test a theory

--- install.sh
+++ install.sh
@@ -7,43 +7,43 @@ VMINSTALL="FALSE"
 RSAUPASS="FALSE"
 for arg in "$@"
 do
-    case $ARGMODE in
-        CHECK)
-            case $arg in
-                -u | --username)
-                ARGMODE="USERNAME"
-                ;;
-                -p | --userpass | --userpassword)
-                ARGMODE="USERPASS"
-                ;;
-                -r | --rootpass | --rootpassword)
-                ARGMODE="ROOTPASS"
-                ;;
-                -vm)
-                VMINSTALL="TRUE"
-                ;;
-                -h | --help | *)
-                echo "This Script is for automating the install of arch linux"
-                echo "-u\t--username\n\tSet the user name of the users account"
-                echo "-p\t--userpass\t--userpassword\n\tSet the user password of the users account"
-                echo "-r\t--rootpass\t--rootpassword\n\tSet the root user's password"
-                echo "-vm\n\tThis is for when instaling to a virtual machine as some items are not needed since host machene takes care of them"
-                echo "-rsau\n\tSet Root user password to be the same as user\n\tNOTE: This is not recomended as this could be a security risk"
-                echo "-h\t--help\n\tshow this text"
-                exit 0
-                ;;
-            esac
-            ;;
-        USERNAME)
-            USERNAME=$arg
-            ;;
-        USERPASS)
-            USERPASS=$arg
-            ;;
-        ROOTPASS)
-            ROOTPASS=$arg
-            ;;
-    esac
+	case $ARGMODE in
+		CHECK)
+			case $arg in
+				-u | --username)
+				ARGMODE="USERNAME"
+				;;
+				-p | --userpass | --userpassword)
+				ARGMODE="USERPASS"
+				;;
+				-r | --rootpass | --rootpassword)
+				ARGMODE="ROOTPASS"
+				;;
+				-vm)
+				VMINSTALL="TRUE"
+				;;
+				-h | --help | *)
+				echo "This Script is for automating the install of arch linux"
+				echo "-u\t--username\n\tSet the user name of the users account"
+				echo "-p\t--userpass\t--userpassword\n\tSet the user password of the users account"
+				echo "-r\t--rootpass\t--rootpassword\n\tSet the root user's password"
+				echo "-vm\n\tThis is for when instaling to a virtual machine as some items are not needed since host machene takes care of them"
+				echo "-rsau\n\tSet Root user password to be the same as user\n\tNOTE: This is not recomended as this could be a security risk"
+				echo "-h\t--help\n\tshow this text"
+				exit 0
+				;;
+			esac
+			;;
+		USERNAME)
+			USERNAME=$arg
+			;;
+		USERPASS)
+			USERPASS=$arg
+			;;
+		ROOTPASS)
+			ROOTPASS=$arg
+			;;
+	esac
 done
 
 ### Varibles to run the rest of the script with
@@ -59,11 +59,11 @@ UEFI='\e[1;34m'
 ### Check for internet Connection
 echo -e "${NOTE}Checking for internet Connection:${CLER}"
 if ping -q -c 1 -W 1 google.com > /dev/null; then
-    echo -e "${SUCC}Connected${CLER}\n"
+	echo -e "${SUCC}Connected${CLER}\n"
 else
-    echo -e "${EROR}NOT CONNECTED${CLER} Please check your connection and try again"
-    # exit with code 1
-    exit 1
+	echo -e "${EROR}NOT CONNECTED${CLER} Please check your connection and try again"
+	# exit with code 1
+	exit 1
 fi
 
 ### Basic setup question if not passed as arguments
@@ -81,11 +81,11 @@ fi
 ### Verify the boot mode
 if [ -d "/sys/firmware/efi" ]
 then
-    echo -e "${UEFI}UEFI boot ${NOTE}mode${CLER}"
-    BOOT="UEFI"
+	echo -e "${UEFI}UEFI boot ${NOTE}mode${CLER}"
+	BOOT="UEFI"
 else
-    echo -e "${LEGA}Legacy boot ${NOTE}mode${CLER}"
-    BOOT="LEGACY"
+	echo -e "${LEGA}Legacy boot ${NOTE}mode${CLER}"
+	BOOT="LEGACY"
 fi
 
 ### update system clock
@@ -94,22 +94,22 @@ timedatectl set-ntp true
 ### Partition, format, and mount the harddrives based on BOOT mode
 if [ $BOOT = "UEFI" ]
 then
-    #sdX1 boot(512MiB)|SdX2 root(whats left after sdX1 or sdx3)|sdX3 swap(4GiB)
-    parted -a optimal -s /dev/sda -- mklabel gpt mkpart primary ext2 0% 512MiB set 1 esp on mkpart primary ext4 512MiB -4GiB mkpart primary linux-swap -4GiB 100% set 3 swap on
-    mkfs.ext2 -F /dev/sda1
-    mkfs.ext4 -F /dev/sda2
-    mkswap /dev/sda3
-    mount /dev/sda2 /mnt
-    mkdir /mnt/boot
-    mount /dev/sda1 /mnt/boot
-    swapon /dev/sda3
+	#sdX1 boot(512MiB)|SdX2 root(whats left after sdX1 or sdx3)|sdX3 swap(4GiB)
+	parted -a optimal -s /dev/sda -- mklabel gpt mkpart primary ext2 0% 512MiB set 1 esp on mkpart primary ext4 512MiB -4GiB mkpart primary linux-swap -4GiB 100% set 3 swap on
+	mkfs.ext2 -F /dev/sda1
+	mkfs.ext4 -F /dev/sda2
+	mkswap /dev/sda3
+	mount /dev/sda2 /mnt
+	mkdir /mnt/boot
+	mount /dev/sda1 /mnt/boot
+	swapon /dev/sda3
 else
-    #sdX1 root(rest minus sdX2)|sdX2 swap(4GiB)
-    parted -a optimal -s /dev/sda -- mklabel msdos mkpart primary ext4 0% -4 GiB set 1 boot on mkpart primary linux swap -4GiB 100% set 2 swap on
-    mkfs.ext4 -F /dev/sda1
-    mkswap /dev/sda2
-    mount /dev/sda1 /mnt
-    swapon /dev/sad2
+	#sdX1 root(rest minus sdX2)|sdX2 swap(4GiB)
+	parted -a optimal -s /dev/sda -- mklabel msdos mkpart primary ext4 0% -4 GiB set 1 boot on mkpart primary linux swap -4GiB 100% set 2 swap on
+	mkfs.ext4 -F /dev/sda1
+	mkswap /dev/sda2
+	mount /dev/sda1 /mnt
+	swapon /dev/sad2
 fi
 
 echo -e "${NOTE}Ranking Pacman Mirrors${CLER}"
