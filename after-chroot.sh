555898c9ec987fe987d83a60c02fe0f7fa86db93
Stephen Pierce <Termanater13@users.noreply.github.com>
2020-07-14 02:52:28 -0400

    Update after-chroot.sh
    added timezone local and way to pass variables gotten from previus script to this

--- after-chroot.sh
+++ after-chroot.sh
@@ -1,6 +1,17 @@
+### Set ARGS for script(after-chroot.sh $1 $2 $3)
+USER=$1
+PASS=$2
+ROOT=$3
+HOST="${USER}-VM"
 #set timezone
+ls -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
 #set localization
+sed 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 #set Network configuration
+
 # no ititramfs needed
 #set users and passwords
 #reboot
