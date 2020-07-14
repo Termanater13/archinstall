b94a1a0cc60b168e34db382e41f9e46ff95d1729
Stephen Pierce <Termanater13@users.noreply.github.com>
2020-07-14 04:48:22 -0400

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -7,7 +7,8 @@ HOST="${USER}-VM"
 ls -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 #set localization
-sed 's/#^en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+#sed 's/#^en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 #set Network configuration
