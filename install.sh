3b7503990cf2629211afc40f5ad4ce64777d3188
Stephen Pierce <Termanater13@users.noreply.github.com>
2020-07-14 03:36:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -116,7 +116,7 @@ function f_FSTAB {
 function f_CHROOT {
 	# add script as a extra argument to run script after root change
 	wget https://raw.githubusercontent.com/Termanater13/archinstall/master/after-chroot.sh -O /mnt/after-chroot.sh
-	arch-chroot /mnt ./after-chroot.sh $USERNAME $USERPASS $ROOTPASS
+	arch-chroot /mnt /bin/bash ./after-chroot.sh $USERNAME $USERPASS $ROOTPASS
 	echo -e "${C_NOTE}[ 11/11]${C_CLER}"
 }
 
